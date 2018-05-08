3d359ffa123d4fe24a7685e7ad916cd97668de04
Jack Fong <captainjack_f@163.com>
2018-05-08 21:21:12 +0800

    Update 钓鱼活动.py

--- 钓鱼活动.py
+++ 钓鱼活动.py
@@ -5,8 +5,7 @@ Created on Mon May  7 21:54:59 2018
 @author: Efun
 """
 
-import MySQLdb as mdb
-#  pandas as pd
+import pandas as pd
 import os
 
 
@@ -34,7 +33,40 @@ for i in loop_folder( os.getcwd()):
     print( str( i + r"_tb_log_fishing.json... Done!" ))
     result = pd.concat( [ result, temp_data])
     
+   
+##行数
+print( len( result)) ## 6896880
 
+## 成功数
+success_num = result[ result.result == 1] 
+gp_success = success_num.groupby( by = 'UserId')  
+gp_success_result = gp_success.size() 
+gp_success_result.to_csv( 'success_result.csv')
+
+## 失败数
+fail_num = result[ result.result == 0] 
+gp_fail = fail_num.groupby( by = 'UserId')  
+gp_fail_result = gp_fail.size() 
+gp_fail_result.to_csv( 'fail_result.csv')
+
+
+## 鱼/宝箱数
+item_num = result[ result.reward > 0] 
+gp_item = item_num.groupby( by = 'reward')  
+gp_item_result = gp_item.size() 
+gp_item_result.to_csv( 'item_result.csv')
+
+
+'''
 writer = pd.ExcelWriter( r'D:\Work\201805\20180508 - 日志\fishing.xlsx')
 data_all.to_excel( writer, index = False, encoding = 'utf-8', sheet_name = 'Sheet')
 writer.save()
+'''
+
+## Json输出
+'''
+json_result = result.to_json( orient='split')  
+fileObject = open( 'json_result.json', 'w')  
+fileObject.write( json_result)  
+fileObject.close() 
+'''
